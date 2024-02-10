b5b4c093fcaab54308591eccd45c6dc18429eeb6
ohseungmin <ohmin2886@gmail.com>
2024-02-10 19:39:03 +0900

    튜플과 배열의 작성법

---
+++ list.ts
@@ -0,0 +1,7 @@
+// 배열은 한개의 타입을 지정해서 쓸 수 있다.
+const list : string[] = ["사과", "배", "오렌지"];
+console.log(list);
+
+// 튜플은 각 요소는 고유의 타입을 가질 수 있다.
+const tuple: [string, number] = ["person", 30];
+console.log(tuple);
